7b7daca6121472afedfb2b0aa758b7a641e71e74
mrstephenneal <stephen.neal14@gmail.com>
2019-07-09 15:38:25 -0400

    MAKE build and push scripts

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker build -t stephenneal/php-laravel:7 ./7/
+docker build -t stephenneal/php-laravel:7.2-fpm ./7.2-fpm/
+docker build -t stephenneal/php-laravel:7.3.6-fpm-v1 ./7.3.6-fpm-v1/
+docker build -t stephenneal/php-laravel:7.3.6-fpm-v2 ./7.3.6-fpm-v2/
+docker build -t stephenneal/php-laravel:7.4-rc-fpm-v1 ./7.4-rc-fpm-v1/
