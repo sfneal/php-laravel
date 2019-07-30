87dad62006ebb97ee6c2e4085f199c62886bae45
mrstephenneal <stephen.neal14@gmail.com>
2019-07-30 15:49:04 -0400

    ADD v3 image builds

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ docker build -t stephenneal/php-laravel:7 ./7/
 docker build -t stephenneal/php-laravel:7.2-fpm ./7.2-fpm/
 docker build -t stephenneal/php-laravel:7.3.6-fpm-v1 ./7.3.6-fpm-v1/
 docker build -t stephenneal/php-laravel:7.3.6-fpm-v2 ./7.3.6-fpm-v2/
+docker build -t stephenneal/php-laravel:7.3.6-fpm-v3 ./7.3.6-fpm-v3/
 docker build -t stephenneal/php-laravel:7.4-rc-fpm-v1 ./7.4-rc-fpm-v1/
--- push.sh
+++ push.sh
@@ -6,4 +6,5 @@ docker push stephenneal/php-laravel:7
 docker push stephenneal/php-laravel:7.2-fpm
 docker push stephenneal/php-laravel:7.3.6-fpm-v1
 docker push stephenneal/php-laravel:7.3.6-fpm-v2
+docker push stephenneal/php-laravel:7.3.6-fpm-v3
 docker push stephenneal/php-laravel:7.4-rc-fpm-v1
