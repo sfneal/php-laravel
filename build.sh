9a1d0d666cdc1610369411c222d8d3fa2885b110
mrstephenneal <stephen.neal14@gmail.com>
2019-09-23 15:34:26 -0400

    MAKE 7.3-fpm-buster-v1 image so that minor patches are automated

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 docker build -t stephenneal/php-laravel:7 ./7/
 docker build -t stephenneal/php-laravel:7.2-fpm ./7.2-fpm/
+docker build -t stephenneal/php-laravel:7.3-fpm-buster-v1 ./7.3-fpm-buster-v1/
 docker build -t stephenneal/php-laravel:7.3.6-fpm-v1 ./7.3.6-fpm-v1/
 docker build -t stephenneal/php-laravel:7.3.6-fpm-v2 ./7.3.6-fpm-v2/
 docker build -t stephenneal/php-laravel:7.3.6-fpm-v3 ./7.3.6-fpm-v3/
--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ sh ./build.sh
 
 docker push stephenneal/php-laravel:7
 docker push stephenneal/php-laravel:7.2-fpm
+docker push stephenneal/php-laravel:7.3-fpm-buster-v1
 docker push stephenneal/php-laravel:7.3.6-fpm-v1
 docker push stephenneal/php-laravel:7.3.6-fpm-v2
 docker push stephenneal/php-laravel:7.3.6-fpm-v3
