10cfb5f5d2f73e4e6054904654245b3a5de5041e
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 13:35:42 -0500

    ADD absolute directory resolving to build and push shell scripts

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-docker build -t stephenneal/php-laravel:7 ./7/
-docker build -t stephenneal/php-laravel:7.2-fpm ./7.2-fpm/
-docker build -t stephenneal/php-laravel:7.3-fpm-buster-v1 ./7.3-fpm-buster-v1/
-docker build -t stephenneal/php-laravel:7.3.6-fpm-v1 ./7.3.6-fpm-v1/
-docker build -t stephenneal/php-laravel:7.3.6-fpm-v2 ./7.3.6-fpm-v2/
-docker build -t stephenneal/php-laravel:7.3.6-fpm-v3 ./7.3.6-fpm-v3/
-docker build -t stephenneal/php-laravel:7.3.7-fpm-v1 ./7.3.7-fpm-v1/
-docker build -t stephenneal/php-laravel:7.3.7-fpm-buster-v1 ./7.3.7-fpm-buster-v1/
-docker build -t stephenneal/php-laravel:7.4-rc-fpm-v1 ./7.4-rc-fpm-v1/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t stephenneal/php-laravel:7 "${DIR}"/7/
+docker build -t stephenneal/php-laravel:7.2-fpm "${DIR}"/7.2-fpm/
+docker build -t stephenneal/php-laravel:7.3-fpm-buster-v1 "${DIR}"/7.3-fpm-buster-v1/
+docker build -t stephenneal/php-laravel:7.3.6-fpm-v1 "${DIR}"/7.3.6-fpm-v1/
+docker build -t stephenneal/php-laravel:7.3.6-fpm-v2 "${DIR}"/7.3.6-fpm-v2/
+docker build -t stephenneal/php-laravel:7.3.6-fpm-v3 "${DIR}"/7.3.6-fpm-v3/
+docker build -t stephenneal/php-laravel:7.3.7-fpm-v1 "${DIR}"/7.3.7-fpm-v1/
+docker build -t stephenneal/php-laravel:7.3.7-fpm-buster-v1 "${DIR}"/7.3.7-fpm-buster-v1/
+docker build -t stephenneal/php-laravel:7.4-rc-fpm-v1 "${DIR}"/7.4-rc-fpm-v1/
--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sh ./build.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sh "${DIR}"/build.sh
 
 docker push stephenneal/php-laravel:7
 docker push stephenneal/php-laravel:7.2-fpm
