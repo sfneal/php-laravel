b4b8d4ce234b08c3107723ac27f5a0b8d6ce7f9f
mrstephenneal <stephen.neal14@gmail.com>
2020-07-30 15:17:12 -0400

    CUT use of 'fpm-buster' images & replaced with 'fpm'
    REFACTOR '-buster' image directories.

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build -t stephenneal/php-laravel:7 "${DIR}"/7/
 docker build -t stephenneal/php-laravel:7.2-fpm "${DIR}"/7.2-fpm/
-docker build -t stephenneal/php-laravel:7.3-fpm-buster-v1 "${DIR}"/7.3-fpm-buster-v1/
+docker build -t stephenneal/php-laravel:7.3-fpm-v1 "${DIR}"/7.3-fpm-v1/
 docker build -t stephenneal/php-laravel:7.3.6-fpm-v1 "${DIR}"/7.3.6-fpm-v1/
 docker build -t stephenneal/php-laravel:7.3.6-fpm-v2 "${DIR}"/7.3.6-fpm-v2/
 docker build -t stephenneal/php-laravel:7.3.6-fpm-v3 "${DIR}"/7.3.6-fpm-v3/
 docker build -t stephenneal/php-laravel:7.3.7-fpm-v1 "${DIR}"/7.3.7-fpm-v1/
-docker build -t stephenneal/php-laravel:7.3.7-fpm-buster-v1 "${DIR}"/7.3.7-fpm-buster-v1/
-docker build -t stephenneal/php-laravel:7.4-fpm-buster-v1 "${DIR}"/7.4-fpm-buster-v1/
-docker build -t stephenneal/php-laravel:7.4-fpm-buster-v2 "${DIR}"/7.4-fpm-buster-v2/
-docker build -t stephenneal/php-laravel:7.4-fpm-buster-v3 "${DIR}"/7.4-fpm-buster-v3/
+docker build -t stephenneal/php-laravel:7.3.7-fpm-v1 "${DIR}"/7.3.7-fpm-v1/
+docker build -t stephenneal/php-laravel:7.4-fpm-v1 "${DIR}"/7.4-fpm-v1/
+docker build -t stephenneal/php-laravel:7.4-fpm-v2 "${DIR}"/7.4-fpm-v2/
+docker build -t stephenneal/php-laravel:7.4-fpm-v3 "${DIR}"/7.4-fpm-v3/
 docker build -t stephenneal/php-laravel:7.4-fpm-composer-v1 "${DIR}"/7.4-fpm-composer-v1/
 docker build -t stephenneal/php-laravel:7.4-fpm-composer-v2 "${DIR}"/7.4-fpm-composer-v2/
 docker build -t stephenneal/php-laravel:7.4-rc-fpm-v1 "${DIR}"/7.4-rc-fpm-v1/
-docker build -t stephenneal/php-laravel:8.0-rc-fpm-buster-v1 "${DIR}"/8.0-rc-fpm-buster-v1/
+docker build -t stephenneal/php-laravel:8.0-rc-fpm-v1 "${DIR}"/8.0-rc-fpm-v1/
--- push.sh
+++ push.sh
@@ -6,16 +6,16 @@ sh "${DIR}"/build.sh
 
 docker push stephenneal/php-laravel:7
 docker push stephenneal/php-laravel:7.2-fpm
-docker push stephenneal/php-laravel:7.3-fpm-buster-v1
+docker push stephenneal/php-laravel:7.3-fpm-v1
 docker push stephenneal/php-laravel:7.3.6-fpm-v1
 docker push stephenneal/php-laravel:7.3.6-fpm-v2
 docker push stephenneal/php-laravel:7.3.6-fpm-v3
 docker push stephenneal/php-laravel:7.3.7-fpm-v1
-docker push stephenneal/php-laravel:7.3.7-fpm-buster-v1
-docker push stephenneal/php-laravel:7.4-fpm-buster-v1
-docker push stephenneal/php-laravel:7.4-fpm-buster-v2
-docker push stephenneal/php-laravel:7.4-fpm-buster-v3
+docker push stephenneal/php-laravel:7.3.7-fpm-v1
+docker push stephenneal/php-laravel:7.4-fpm-v1
+docker push stephenneal/php-laravel:7.4-fpm-v2
+docker push stephenneal/php-laravel:7.4-fpm-v3
 docker push stephenneal/php-laravel:7.4-fpm-composer-v1
 docker push stephenneal/php-laravel:7.4-fpm-composer-v2
 docker push stephenneal/php-laravel:7.4-rc-fpm-v1
-docker push stephenneal/php-laravel:8.0-rc-fpm-buster-v1
+docker push stephenneal/php-laravel:8.0-rc-fpm-v1
