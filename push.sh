4fa2db47102b420f2beb2e2983cf6734c2fa99ea
sfneal <stephen.neal14@gmail.com>
2024-05-30 13:39:05 -0400

    CUT rc images from build/push scripts

--- push.sh
+++ push.sh
@@ -31,16 +31,12 @@ if [ "$TAG" != null ]
     docker push stephenneal/php-laravel:7.4-fpm-v3.1
     docker push stephenneal/php-laravel:7.4-fpm-composer-v1
     docker push stephenneal/php-laravel:7.4-fpm-composer-v2
-    docker push stephenneal/php-laravel:7.4-rc-fpm-v1
     docker push stephenneal/php-laravel:8.0-fpm-v1
     docker push stephenneal/php-laravel:8.0-fpm-v2
     docker push stephenneal/php-laravel:8.0-fpm-v3
     docker push stephenneal/php-laravel:8.1-fpm-v1
     docker push stephenneal/php-laravel:8.1-fpm-v2
     docker push stephenneal/php-laravel:8.1-fpm-v3
-    docker push stephenneal/php-laravel:8.1-rc-fpm-v1
-    docker push stephenneal/php-laravel:8.1-rc-fpm-v1.1
-    docker push stephenneal/php-laravel:8.1-rc-fpm-v2
     docker push stephenneal/php-laravel:8.2-fpm-v1
     docker push stephenneal/php-laravel:8.2-fpm-v2
     docker push stephenneal/php-laravel:8.2-fpm-v3
