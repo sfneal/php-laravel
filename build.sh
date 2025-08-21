6287edf53c46a3ab69c8031c8e30f73d7319db9c
Stephen Neal <stephen.neal14@gmail.com>
2025-08-21 14:57:01 -0400

    Remove PHP 7.3 & below base images (#14)
    * CUT PHP 7.3 & below images
    
    * CUT PHP 7.3 & below images from build/push scripts

--- build.sh
+++ build.sh
@@ -37,14 +37,6 @@ if [ "$TAG" != null ]
 
   # Build all images
   else
-    docker build -t stephenneal/php-laravel:7 "${DIR}"/7/
-    docker build -t stephenneal/php-laravel:7.2-fpm "${DIR}"/7.2-fpm/
-    docker build -t stephenneal/php-laravel:7.3-fpm-v1 "${DIR}"/7.3-fpm-v1/
-#    docker build -t stephenneal/php-laravel:7.3.6-fpm-v1 "${DIR}"/7.3.6-fpm-v1/
-#    docker build -t stephenneal/php-laravel:7.3.6-fpm-v2 "${DIR}"/7.3.6-fpm-v2/
-#    docker build -t stephenneal/php-laravel:7.3.6-fpm-v3 "${DIR}"/7.3.6-fpm-v3/
-    docker build -t stephenneal/php-laravel:7.3.7-fpm-v1 "${DIR}"/7.3.7-fpm-v1/
-    docker build -t stephenneal/php-laravel:7.3.7-fpm-v1 "${DIR}"/7.3.7-fpm-v1/
     docker build -t stephenneal/php-laravel:7.4-fpm-v1 "${DIR}"/7.4-fpm-v1/
     docker build -t stephenneal/php-laravel:7.4-fpm-v2 "${DIR}"/7.4-fpm-v2/
     docker build -t stephenneal/php-laravel:7.4-fpm-v3 "${DIR}"/7.4-fpm-v3/
